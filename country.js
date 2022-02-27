d428e6f9575c0cd793266ec24e95327033d94032
majharul224 <majharulislam2225@gmail.com>
2022-02-27 19:18:29 +0600

    first commit

--- country.js
+++ country.js
@@ -7,11 +7,16 @@
 
 lodeCountry();
 const countryDisplay =(country)=>{
-    //console.log(country)
+    console.log(country)
     const countryDiv= document.getElementById('countrys');
     for(countries of country){
-        const div =document.createElement('div');
-        
+       // const div =document.createElement('div');
+      //  div.classList.add('style')
+        const h3 = document.createElement('h3');
+        h3.innerText= country.countries.name;
+        countryDiv.appendChild(h3);
+
+
 
     }
 } 
